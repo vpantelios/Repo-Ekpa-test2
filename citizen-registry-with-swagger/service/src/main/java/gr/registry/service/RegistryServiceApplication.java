ef692ea252b7dce805ce8c6e85badcc9eec079f6
vpantelios <bplemans2k@gmail.com>
2025-08-18 23:13:09 +0300

    add entityscan gr.registry.domain

--- citizen-registry-with-swagger/service/src/main/java/gr/registry/service/RegistryServiceApplication.java
+++ citizen-registry-with-swagger/service/src/main/java/gr/registry/service/RegistryServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** Εκκίνηση RESTful υπηρεσίας μητρώου πολιτών. */
 @SpringBootApplication(scanBasePackages = "gr.registry")
+@EntityScan(basePackages = "gr.registry.domain")
 public class RegistryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryServiceApplication.class, args);
